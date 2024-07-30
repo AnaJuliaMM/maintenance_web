b9528083922d8ef16b46aadfeb8ec46454fdbcdc
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-07-29 22:53:35 -0300

    chore: start frontend project

---
+++ frontend/src/app/components/HomeCard.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { LuWarehouse } from 'react-icons/lu'
+
+type HomeCardProps =  {
+  value:number;
+  category:string;
+  color:string;
+  icon: React.ReactNode;
+
+}
+
+export const HomeCard:React.FC<HomeCardProps> = ({value, category, color, icon}) => {
+  return (
+    <div className={`flex justify-between p-6 ${color} rounded-md items-center`}>
+    <div className="flex flex-col">
+      <strong>{value}</strong>
+      <span>{category}</span>
+    </div>
+    {icon}
+  </div>
+  )
+}
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Controle de Manutenção",
+  description: "Sistema para controle e gerenciamento de manutenções em uma indústria",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-900">{children}</body>
+    </html>
+  );
+}
