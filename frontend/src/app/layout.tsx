69133c742224cbdcd1636fde15bddf1400ad5d07
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-07-30 19:57:58 -0300

    feat: add aside and footer as a fixed layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,11 +2,20 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { GoTools } from "react-icons/go";
+import { LuWarehouse } from "react-icons/lu";
+import { RxDashboard } from "react-icons/rx";
+import { FaUsers } from "react-icons/fa";
+import { IoIosListBox } from "react-icons/io";
+
+import { Aside } from "./components/Aside";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Controle de Manutenção",
-  description: "Sistema para controle e gerenciamento de manutenções em uma indústria",
+  description:
+    "Sistema para controle e gerenciamento de manutenções em uma indústria",
 };
 
 export default function RootLayout({
@@ -14,9 +23,45 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const navItems = [
+    {
+      value: "Dashboards",
+      href: "",
+      icon: <RxDashboard />,
+    },
+    {
+      value: "Ambientes",
+      href: "",
+      icon: <LuWarehouse />,
+    },
+    {
+      value: "Equipamentos",
+      href: "",
+      icon: <GoTools />,
+    },
+    {
+      value: "Manutenções",
+      href: "",
+      icon: <IoIosListBox />,
+    },
+    {
+      value: "Usuários",
+      href: "",
+      icon: <FaUsers />,
+    },
+  ];
+
   return (
     <html lang="en">
-      <body className="bg-gray-900">{children}</body>
+      <body className="bg-gray-900 h-screen flex flex-col">
+        <div className="flex-1 flex">
+          <Aside navItems={navItems} />
+          {children}
+        </div>
+        <footer className="p-6  text-center">
+          Todos os direitos reservados &copy; 2024
+        </footer>
+      </body>
     </html>
   );
 }
