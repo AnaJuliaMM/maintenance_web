f1c062bf7173601935a5a3aab4cfe04d857f439d
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-07-30 21:12:49 -0300

    feat: add logo at aside component

--- frontend/src/app/components/Aside.tsx
+++ frontend/src/app/components/Aside.tsx
@@ -1,26 +1,61 @@
 import React from "react";
 import Image from "next/image";
 
+import { GoTools } from "react-icons/go";
+import { LuWarehouse } from "react-icons/lu";
+import { RxDashboard } from "react-icons/rx";
+import { FaUsers } from "react-icons/fa";
+import { IoIosListBox } from "react-icons/io";
+
 import { NavIconItem } from "./NavIconItem";
 import { NavIconItemProps } from "./NavIconItem";
 
-type AsideProps = {
-  navItems: NavIconItemProps[];
-};
+export const Aside = () => {
+  const navItems: NavIconItemProps[] = [
+    {
+      value: "Dashboards",
+      href: "",
+      icon: <RxDashboard />,
+    },
+    {
+      value: "Ambientes",
+      href: "",
+      icon: <LuWarehouse />,
+    },
+    {
+      value: "Equipamentos",
+      href: "",
+      icon: <GoTools />,
+    },
+    {
+      value: "Manutenções",
+      href: "",
+      icon: <IoIosListBox />,
+    },
+    {
+      value: "Usuários",
+      href: "",
+      icon: <FaUsers />,
+    },
+  ];
 
-export const Aside: React.FC<AsideProps> = ({ navItems }) => {
   return (
     <aside className="w-64 p-6 bg-gray-950/35">
       <Image
-        src={"/image/image.png"}
+        src={"/image/logo.svg"}
         alt="Logo"
-        width={300}
-        height={300}
+        width={200}
+        height={200}
         className="w-full"
       ></Image>
       <nav className="flex flex-col space-y-4 mt-8">
-        {navItems.map((item) => (
-          <NavIconItem href={item.href} icon={item.icon} value={item.value} />
+        {navItems.map((item, index) => (
+          <NavIconItem
+            href={item.href}
+            icon={item.icon}
+            value={item.value}
+            key={index}
+          />
         ))}
       </nav>
     </aside>
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,12 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { GoTools } from "react-icons/go";
-import { LuWarehouse } from "react-icons/lu";
-import { RxDashboard } from "react-icons/rx";
-import { FaUsers } from "react-icons/fa";
-import { IoIosListBox } from "react-icons/io";
-
 import { Aside } from "./components/Aside";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,39 +17,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const navItems = [
-    {
-      value: "Dashboards",
-      href: "",
-      icon: <RxDashboard />,
-    },
-    {
-      value: "Ambientes",
-      href: "",
-      icon: <LuWarehouse />,
-    },
-    {
-      value: "Equipamentos",
-      href: "",
-      icon: <GoTools />,
-    },
-    {
-      value: "Manutenções",
-      href: "",
-      icon: <IoIosListBox />,
-    },
-    {
-      value: "Usuários",
-      href: "",
-      icon: <FaUsers />,
-    },
-  ];
-
   return (
     <html lang="en">
       <body className="bg-gray-900 h-screen flex flex-col">
         <div className="flex-1 flex">
-          <Aside navItems={navItems} />
+          <Aside />
           {children}
         </div>
         <footer className="p-6  text-center">
