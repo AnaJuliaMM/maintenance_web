1e3b506a9f0e1c7c07273213607cce705c3b24ae
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-08-01 22:03:05 -0300

    refactor: move home page to directory

--- frontend/src/app/(routes)/page.tsx
+++ frontend/src/app/(routes)/page.tsx
@@ -5,9 +5,10 @@ import { IoIosListBox } from "react-icons/io";
 import { MdOutlineEdit } from "react-icons/md";
 import { MdDeleteOutline } from "react-icons/md";
 
-import { HomeCard } from "./components/HomeCard";
-import { DataTable } from "./components/DataTable";
-import BarChartComponent from "./components/BarChart";
+import { HomeCard } from "../components/HomeCard";
+import { DataTable } from "../components/DataTable";
+import BarChartComponent from "../components/BarChart";
+import { Aside } from "../components/Aside";
 
 export default function Home() {
   const tableRows = [
@@ -44,39 +45,10 @@ export default function Home() {
   ];
 
   return (
-    <main className="flex-1 flex flex-col p-10 ml-60 mb-20">
+    <main className="flex flex-col">
       <h1 className="text-3xl pb-6 text-center">Dashboard</h1>
-      <div className="max-h-svh overflow-y-auto">
-        <div className="grid grid-cols-4 gap-3 pb-5">
-          <HomeCard
-            value={200}
-            category="Ambientes"
-            color="bg-pink-600/90"
-            icon={<LuWarehouse size={30} />}
-          />
-          <HomeCard
-            value={1454}
-            category="Equipamentos"
-            color="bg-blue-500/60"
-            icon={<GoTools size={30} />}
-          />
-          <HomeCard
-            value={100}
-            category="Manutenções"
-            color="bg-purple-500/60"
-            icon={<IoIosListBox size={30} />}
-          />
-          <HomeCard
-            value={150}
-            category="Usuários"
-            color="bg-slate-200/60"
-            icon={<FaUsers size={30} />}
-          />
-        </div>
-        <DataTable />
-        <BarChartComponent />
-      </div>
+
     </main>
   );
 }
-6;
+
