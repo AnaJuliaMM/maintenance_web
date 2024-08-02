ee037811a117106efd87960b3b8eb4e66b145772
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-08-01 22:03:23 -0300

    feat: create file for machine page

---
+++ frontend/src/app/(routes)/machine/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Machine() {
+  return (
+    <main className="flex flex-col p-6 w-svw">
+      <h1 className="p-2 opacity-60 text-blue-100 text-xl border-b">
+        Máquinas
+      </h1>
+    </main>
+  );
+}
