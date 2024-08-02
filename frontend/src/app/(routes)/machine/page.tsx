c7785d3fde08d5b1d0983b90d926d50b8c5cf598
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-08-01 22:52:12 -0300

    feat: add prime react table for machines

--- frontend/src/app/(routes)/machine/page.tsx
+++ frontend/src/app/(routes)/machine/page.tsx
@@ -2,18 +2,61 @@ import React from "react";
 import { Title } from "@/app/components/Title";
 import { IoAddCircle } from "react-icons/io5";
 
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+
+
+
+
 export default function Machine() {
+
+    const products = [
+        {
+            "code": 1,
+            "name": "ana",
+            "category": "a",
+            "quantity": 5
+        }
+
+    ]
   return (
     <main className="flex flex-col p-6 w-svw gap-4">
       <Title>Máquinas</Title>
       <section className="flex gap-2 items-center cursor-pointer">
         <div className="flex gap-2 justify-center items-center bg-blue-500 py-2 px-4 rounded-lg font-semibold">
-     
-          <IoAddCircle size={30} /> 
+          <IoAddCircle size={30} />
           Cadastrar máquina
         </div>
       </section>
-      <section className="bg-red-700">tabela</section>
+      <section className="flex">
+        <DataTable value={products} tableStyle={{ minWidth: "40rem"}} className="bg-gray-500 p-2 rounded-lg">
+          <Column
+            field="code"
+            header="Code"
+            sortable
+            style={{ width: "25%", backgroundColor: "red" }}
+
+          ></Column>
+          <Column
+            field="name"
+            header="Name"
+            sortable
+            style={{ width: "25%" }}
+          ></Column>
+          <Column
+            field="category"
+            header="Category"
+            sortable
+            style={{ width: "25%" }}
+          ></Column>
+          <Column
+            field="quantity"
+            header="Quantity"
+            sortable
+            style={{ width: "25%" }}
+          ></Column>
+        </DataTable>
+      </section>
     </main>
   );
 }
