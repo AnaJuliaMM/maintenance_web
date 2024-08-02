56b4a2607acd6536181ef942dff90b6e299d7bfd
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-08-01 22:33:39 -0300

    feat: create button to add new machine record
    Create div to perform like a button with an icon and text

--- frontend/src/app/(routes)/machine/page.tsx
+++ frontend/src/app/(routes)/machine/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { Title } from "@/app/components/Title";
+import { IoAddCircle } from "react-icons/io5";
 
 export default function Machine() {
   return (
-    <main className="flex flex-col p-6 w-svw">
-      <h1 className="p-2 opacity-60 text-blue-100 text-xl border-b">
-        Máquinas
-      </h1>
+    <main className="flex flex-col p-6 w-svw gap-4">
+      <Title>Máquinas</Title>
+      <section className="flex gap-2 items-center cursor-pointer">
+        <div className="flex gap-2 justify-center items-center bg-blue-500 py-2 px-4 rounded-lg font-semibold">
+     
+          <IoAddCircle size={30} /> 
+          Cadastrar máquina
+        </div>
+      </section>
+      <section className="bg-red-700">tabela</section>
     </main>
   );
 }
