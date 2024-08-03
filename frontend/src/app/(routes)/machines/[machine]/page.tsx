a20b3c0a6ac9ff816e2af9fb03d85687dee7334f
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-08-02 21:18:42 -0300

    feat: change machine directory name

---
+++ frontend/src/app/(routes)/machines/[machine]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+export default function Home() {
+  // const router = useRouter();
+  // const { id } = router.query;
+
+  return (
+    <main className="flex flex-col">
+      <h1 className="text-3xl pb-6 text-center">Dashboard </h1>
+    </main>
+  );
+}
