22ee1d180e99da4aede85bd664404f9b288cf42b
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-08-05 17:16:53 -0300

    feat(machine): add params to receive machine serialNumber
    Receive machine serialNumber to get machine details

--- frontend/src/app/(routes)/machines/[machine]/page.tsx
+++ frontend/src/app/(routes)/machines/[machine]/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 import React from "react";
+import { useRouter, useSearchParams } from 'next/navigation';
+import { machineList } from "@/app/constants/machine";
+import { MachineType } from "@/app/types/machine";
 
-export default function Home() {
-  // const router = useRouter();
-  // const { id } = router.query;
+type Params = {
+  machine: string
+}
+
+interface MachineDetailProps {
+  params : Params;
+}
+
+export default function MachineDetail({ params }: MachineDetailProps) {
+  const serialNumber = params.machine;
+  const machine:MachineType  = machineList.filter((machine)=> machine.serialNumber === params.machine)[0]
+  console.log(machine);
+  
 
   return (
     <main className="flex flex-col">
-      <h1 className="text-3xl pb-6 text-center">Dashboard </h1>
+      <h1 className="text-3xl pb-6 text-center">Dashboard {serialNumber}</h1>
+      <h1 className="text-3xl pb-6 text-center">{machine.location} </h1>
     </main>
   );
 }
