c0c628cd0e8ccf9860e8a350e1c60aafe4f1a617
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-08-05 17:48:31 -0300

    feat: add data table and image galery in machine detail page

--- frontend/src/app/(routes)/machines/[machine]/page.tsx
+++ frontend/src/app/(routes)/machines/[machine]/page.tsx
@@ -1,27 +1,93 @@
 "use client";
 import React from "react";
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+
 import { machineList } from "@/app/constants/machine";
 import { MachineType } from "@/app/types/machine";
+import { Title } from "@/app/components/Title";
+import { Galleria } from "primereact/galleria";
 
 type Params = {
-  machine: string
-}
+  machine: string;
+};
 
 interface MachineDetailProps {
-  params : Params;
+  params: Params;
 }
 
 export default function MachineDetail({ params }: MachineDetailProps) {
-  const serialNumber = params.machine;
-  const machine:MachineType  = machineList.filter((machine)=> machine.serialNumber === params.machine)[0]
-  console.log(machine);
-  
+  const machineDetail: MachineType[] = machineList.filter(
+    (machine: MachineType) => machine.serialNumber === params.machine
+  );
+  console.log(machineDetail);
+
+  const itemTemplate = (item: MachineType) => {
+    return (
+      <img
+        src={item.imagesUrl}
+        alt={`Imagem relacionada à máquina ${item.name}`}
+        style={{ width: "100%" }}
+      />
+    );
+  };
 
   return (
-    <main className="flex flex-col">
-      <h1 className="text-3xl pb-6 text-center">Dashboard {serialNumber}</h1>
-      <h1 className="text-3xl pb-6 text-center">{machine.location} </h1>
+    <main className="flex flex-col p-6 w-svw gap-4 h-fit">
+      <Title> {machineDetail[0].name}</Title>
+
+      <DataTable
+        value={machineDetail}
+        className="flex justify-center items-center bg-zinc-400/10 rounded-lg p-2 p-datatable"
+        tableStyle={{ width: "45rem", lineBreak: "anywhere" }}
+      >
+        <Column
+          field="serialNumber"
+          header="Série"
+          sortable
+          className="p-datatable-column"
+          style={{ width: "20%" }}
+        ></Column>
+        <Column
+          field="name"
+          header="Nome"
+          sortable
+          className="p-datatable-column"
+          style={{ width: "20%" }}
+        ></Column>
+        <Column
+          field="model"
+          header="Modelo"
+          sortable
+          className="p-datatable-column"
+          style={{ width: "20%" }}
+        ></Column>
+
+        <Column
+          field="manufactureDate"
+          header="Fabricação"
+          sortable
+          className="p-datatable-column"
+          style={{ width: "20%" }}
+        ></Column>
+        <Column
+          field="location"
+          header="Localização"
+          sortable
+          className="p-datatable-column"
+          style={{ width: "20%" }}
+        ></Column>
+      </DataTable>
+
+      <Galleria
+        value={machineDetail}
+        numVisible={5}
+        style={{ maxWidth: "640px" }}
+        item={itemTemplate}
+        thumbnail={itemTemplate}
+      />
     </main>
   );
 }
