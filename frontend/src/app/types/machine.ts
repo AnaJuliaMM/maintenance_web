2b4843c0aa6e0d9257042a7a9f8dc400998d66c0
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-08-07 17:30:01 -0300

    chore: set imageUrl machine type as string array

--- frontend/src/app/types/machine.ts
+++ frontend/src/app/types/machine.ts
@@ -1,11 +1,9 @@
 export type MachineType = {
-    name: string;
-    type: string;
-    model: string;
-    manufactureDate: string;
-    serialNumber: string;
-    location: string;
-    imageUrl: string
-  };
-
-
+  name: string;
+  type: string;
+  model: string;
+  manufactureDate: string;
+  serialNumber: string;
+  location: string;
+  imagesUrl: string[];
+};
