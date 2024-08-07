add620f75b0a70ccd0c7e83c4276e3660d7be8ca
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-08-07 17:34:47 -0300

    feat: update machine detail page to handle imageUrl as an array
    Set machine object as a single item and use imageUrl array to render images gallery

--- frontend/src/app/(routes)/machines/[machine]/page.tsx
+++ frontend/src/app/(routes)/machines/[machine]/page.tsx
@@ -19,75 +19,122 @@ interface MachineDetailProps {
 }
 
 export default function MachineDetail({ params }: MachineDetailProps) {
-  const machineDetail: MachineType[] = machineList.filter(
+  const machineDetail: MachineType = machineList.filter(
     (machine: MachineType) => machine.serialNumber === params.machine
-  );
-  console.log(machineDetail);
+  )[0];
 
-  const itemTemplate = (item: MachineType) => {
+  const itemTemplate = (item: string) => {
     return (
       <img
-        src={item.imageUrl}
-        alt={`Imagem relacionada à máquina ${item.name}`}
-        style={{ width: "100%" }}
+        src={item}
+        alt={`Imagem relacionada ao equipamento`}
+        style={{ width: "100%", borderRadius: ".5rem" }}
       />
     );
   };
 
+  const thumbnailTemplate = (item: string) => {
+    return <img src={item} alt={`Imagem relacionada ao equipamento`} />;
+  };
+
   return (
     <main className="flex flex-col p-6 w-svw gap-4 h-fit">
-      <Title> {machineDetail[0].name}</Title>
+      <section>
+        <Title> {machineDetail.name}</Title>
+        <div className="flex items-center gap-1 ">
+          <Galleria
+            value={machineDetail.imagesUrl}
+            numVisible={5}
+            circular
+            style={{ maxWidth: "20rem" }}
+            item={itemTemplate}
+            thumbnail={thumbnailTemplate}
+            showItemNavigators
+            showItemNavigatorsOnHover
+            showIndicators
+            showThumbnails={false}
+            className="bg-[var(--surface-card)] p-4 rounded-lg w-1/2"
+          />
+          <div className="flex flex-col justify-center gap-1 bg-zinc-400/10 rounded-lg p-4 w-1/2">
+            <div>
+              <span className="font-bold">Nome:</span> {machineDetail.name}
+            </div>
+            <div>
+              <span className="font-bold">Tipo :</span> {machineDetail.type}
+            </div>
+            <div>
+              <span className="font-bold">Modelo:</span> {machineDetail.model}
+            </div>
+            <div>
+              <span className="font-bold">Localização:</span>{" "}
+              {machineDetail.location}
+            </div>
+            <div>
+              <span className="font-bold">Número de Série :</span>{" "}
+              {machineDetail.serialNumber}
+            </div>
+            <div>
+              <span className="font-bold">Data de fabricação :</span>{" "}
+              {machineDetail.manufactureDate}
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <h1 className="p-4  text-blue-50/95 text-xl"> Peças e Materiais</h1>
+      </section>
+      <section>
+        <h1 className="p-4  text-blue-50/95 text-xl">
+          {" "}
+          Histórico de Manutenções
+        </h1>
+        <DataTable
+          value={machineList}
+          paginator
+          rows={8}
+          paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+          currentPageReportTemplate="{last} de {totalRecords}"
+          className="bg-zinc-400/10 rounded-lg p-4 p-datatable"
+          tableStyle={{ width: "55rem", lineBreak: "anywhere" }}
+        >
+          <Column
+            field="serialNumber"
+            header="Série"
+            sortable
+            className="p-datatable-column"
+            style={{ width: "20%" }}
+          ></Column>
+          <Column
+            field="name"
+            header="Nome"
+            sortable
+            className="p-datatable-column"
+            style={{ width: "20%" }}
+          ></Column>
+          <Column
+            field="model"
+            header="Modelo"
+            sortable
+            className="p-datatable-column"
+            style={{ width: "20%" }}
+          ></Column>
 
-      <DataTable
-        value={machineDetail}
-        className="flex justify-center items-center bg-zinc-400/10 rounded-lg p-2 p-datatable"
-        tableStyle={{ width: "45rem", lineBreak: "anywhere" }}
-      >
-        <Column
-          field="serialNumber"
-          header="Série"
-          sortable
-          className="p-datatable-column"
-          style={{ width: "20%" }}
-        ></Column>
-        <Column
-          field="name"
-          header="Nome"
-          sortable
-          className="p-datatable-column"
-          style={{ width: "20%" }}
-        ></Column>
-        <Column
-          field="model"
-          header="Modelo"
-          sortable
-          className="p-datatable-column"
-          style={{ width: "20%" }}
-        ></Column>
-
-        <Column
-          field="manufactureDate"
-          header="Fabricação"
-          sortable
-          className="p-datatable-column"
-          style={{ width: "20%" }}
-        ></Column>
-        <Column
-          field="location"
-          header="Localização"
-          sortable
-          className="p-datatable-column"
-          style={{ width: "20%" }}
-        ></Column>
-      </DataTable>
-
-      <Galleria
-        value={machineDetail}
-        numVisible={5}
-        style={{ maxWidth: "640px" }}
-        item={itemTemplate}
-        thumbnail={itemTemplate}
-      />
+          <Column
+            field="manufactureDate"
+            header="Fabricação"
+            sortable
+            className="p-datatable-column"
+            style={{ width: "20%" }}
+          ></Column>
+          <Column
+            field="location"
+            header="Localização"
+            sortable
+            className="p-datatable-column"
+            style={{ width: "20%" }}
+          ></Column>
+        </DataTable>
+      </section>
     </main>
   );
 }
