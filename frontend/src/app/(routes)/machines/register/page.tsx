a8c1fb7772a7a68a11538f9e9cb53b14bc880f14
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-08-09 21:19:07 -0300

    feat: add form to register a new machine

--- frontend/src/app/(routes)/machines/register/page.tsx
+++ frontend/src/app/(routes)/machines/register/page.tsx
@@ -1,26 +1,116 @@
+"use client";
 import React, { useState } from "react";
-import { Title } from "@/app/components/Title";
 
 import { FileUpload } from "primereact/fileupload";
 
+import { Title } from "@/app/components/Title";
+import { machineType } from "@/app/types/machine";
+
 export default function Home() {
+  const [formData, setFormData] = useState<machineType>({
+    name: "",
+    type: "",
+    model: "",
+    manufactureDate: "",
+    serialNumber: "",
+    location: "",
+    imagesUrl: [],
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <main className="flex flex-col p-6 w-svw gap-4 h-fit">
       <Title>Cadastrar Máquina</Title>
-      <section className="flex justify-items-end gap-2 items-center cursor-pointer">
-        <div className="bg-zinc-400/10 p-8 rounded-lg mb-4">
+      <form className="register-form">
+        <div>
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="type">Tipo:</label>
+          <input
+            type="text"
+            id="type"
+            name="type"
+            value={formData.type}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="model">Modelo:</label>
+          <input
+            type="text"
+            id="model"
+            name="model"
+            value={formData.model}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="manufactureDate">Data de Fabricação:</label>
+          <input
+            type="date"
+            id="manufactureDate"
+            name="manufactureDate"
+            value={formData.manufactureDate}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="serialNumber">Numero de Série:</label>
+          <input
+            type="text"
+            id="serialNumber"
+            name="serialNumber"
+            value={formData.serialNumber}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="location">Localização:</label>
+          <input
+            type="text"
+            id="location"
+            name="location"
+            value={formData.location}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="location">Imagens:</label>
           <FileUpload
             name="demo[]"
             url={"/api/upload"}
             multiple
             accept="image/*"
             maxFileSize={1000000}
-            emptyTemplate={
-              <p className="m-0">Faça o upload de imagens.</p>
-            }
+            emptyTemplate={<p className="m-0">Faça o upload de imagens.</p>}
+            className="w-3/4 p-4 mb-4 bg-gray-200 rounded-sm text-black"
           />
         </div>
-      </section>
+        <button type="submit" className="flex gap-2 justify-center items-center bg-blue-500 py-2 px-4 rounded-lg font-semibold text-sm">Submit</button>
+      </form>
     </main>
   );
 }
