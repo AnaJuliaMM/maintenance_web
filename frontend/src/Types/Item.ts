42406871fd54518dd1e7c53c447503eeccc89040
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-25 22:48:28 -0300

    feat: create type for item

---
+++ frontend/src/Types/Item.ts
@@ -0,0 +1,11 @@
+export type Item = {
+    id: number;
+    name: string | null;
+    description: string | null;
+    type: string | null;
+    acquisitionDate: string | null;
+    supplier: string | null;
+    quantity: number;
+    status: string | null;
+  };
+  
