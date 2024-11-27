7c821005b749aea97aeb223f3e4baac2485ea98b
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-27 07:06:18 -0300

    refactor: move type outside app directory

---
+++ frontend/src/Types/machine.ts
@@ -0,0 +1,10 @@
+export type machineType = {
+  id?: number;
+  serialNumber?: string;
+  name: string;
+  model?: string;
+  manufactureDate?: string;
+  location?: string;
+  category?: string;
+  imagesUrl?: string[];
+};
--- frontend/src/app/types/machine.ts
+++
@@ -1,9 +0,0 @@
-export type machineType = {
-  name: string;
-  type: string;
-  model: string;
-  manufactureDate: string;
-  serialNumber: string;
-  location: string;
-  imagesUrl: string[];
-};
