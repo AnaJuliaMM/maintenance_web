c7a5008a991788e38fd40bcc10d1f7ebcbab28fe
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-27 23:02:12 -0300

    style: style input component

--- frontend/src/components/InputLabel.tsx
+++ frontend/src/components/InputLabel.tsx
@@ -4,19 +4,13 @@ interface InputLabelProps {
   id: string;
   type: string;
   label: string;
-  value: string;
+  value?: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputLabel = ({
-  id,
-  type,
-  label,
-  value,
-  onChange,
-}: InputLabelProps) => {
+const InputLabel = ({ id, type, label, value, onChange }: InputLabelProps) => {
   return (
-    <>
+    <div className="flex flex-col items-start w-full">
       <label htmlFor={id} className="block font-medium">
         {label}:
       </label>
@@ -27,9 +21,9 @@ const InputLabel = ({
         value={value}
         onChange={onChange}
         required
-        className="w-full"
+        className="flex bg-blue-100 rounded-sm h-10 w-full p-1 text-gray-900 font-semibold"
       />
-    </>
+    </div>
   );
 };
 
