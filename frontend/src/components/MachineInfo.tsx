00063f1ab3129ae402089fe35ecad885e8557625
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-11-28 23:34:30 -0300

    feat: create component to handle machin info

---
+++ frontend/src/components/MachineInfo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface MachineInfoProps {
+  label: string;
+  value?: string | JSX.Element;
+}
+
+export default function MachineInfo({ label, value }: MachineInfoProps) {
+  return (
+    <div className="flex justify-between">
+      <span className="font-bold">{label}:</span> {value}
+    </div>
+  );
+}
