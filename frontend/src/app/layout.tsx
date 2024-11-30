529d7d1d84abb023f444df1fcfa8eeb7a5967e47
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-29 21:50:32 -0300

    fix: fix aside path import

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { Aside } from "./components/Aside";
+import { Aside } from "@/components/Aside";
 
 const inter = Inter({ subsets: ["latin"] });
 
