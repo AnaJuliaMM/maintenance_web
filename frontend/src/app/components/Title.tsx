affc41645f18e30dc8015e1bf774ed0a91d76f2f
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-11-29 21:54:33 -0300

    feat: move components to external components directory

--- frontend/src/app/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type TitleProps = {
-  children: React.ReactNode;
-};
-
-export const Title: React.FC<TitleProps> = ({ children }) => {
-  return <h1 className="text-blue-100 text-2xl font-bold py-5 text-center">{children}</h1>;
-};
