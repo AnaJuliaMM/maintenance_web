2376cc171a654f44fd022ceda2a7c9d53204a4b6
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-12-02 19:35:13 -0300

    feat: config login route

--- frontend/src/app/(routes)/page.tsx
+++ frontend/src/app/(routes)/page.tsx
@@ -1,65 +1,107 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { AxiosError, AxiosStatic } from "axios";
 
 import InputLabel from "@/components/InputLabel";
+import { authType } from "@/types/authType";
+import AuthService from "@/services/auth";
+import { RiLoader2Fill } from "react-icons/ri";
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const router = useRouter();
+  const [formData, setFormData] = useState<authType>({
+    username: "",
+    password: "",
+  });
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  /**
+   * Função para lidar com as mudanças nos campos de entrada de um formulário.
+   * Atualiza o estado `formData` com o valor do campo alterado.
+   * Para campos de texto e numeros, o valor é armazenado diretamente.
+   *
+   * @param {React.ChangeEvent<HTMLInputElement | HTMLSelectElement>} e - O evento de mudança no campo de entrada.
+   */
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  /**
+   * Função para lidar com o envio do formulário.
+   * Previne o envio padrão, formata o payload e envia os dados para a API e lida com a resposta.
+   *
+   * @param {React.FormEvent<HTMLFormElement>} e - O evento de envio do formulário.
+   */
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Aqui você pode adicionar a lógica para autenticação
-    console.log("Email:", username);
-    console.log("Password:", password);
+    try {
+      setLoading(true);
+      await AuthService.login("", formData);
+      router.push("/report");
+    } catch (error: any) {
+      if (error.response && error.response.status === 401) {
+        setError("Usuário ou senha inválidos.");
+      } else {
+        setError(`Falha ao enviar os dados: ${error.message || error}`);
+      }
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100 w-full">
-      <div className=" p-8 rounded-lg shadow-lg w-80">
-        <form onSubmit={handleSubmit}>
-          <InputLabel
-            id="username"
-            type="text"
-            label="Usuário"
-            value={username}
-            onChange={(
-              event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-            ) => {
-              setUsername(event.target.value);
-            }}
-          />
-          <InputLabel
-            id="password"
-            type="password"
-            label="Senha"
-            value={password}
-            onChange={(
-              event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-            ) => {
-              setPassword(event.target.value);
-            }}
-          />
+    <main className="flex flex-col justify-center items-center p-6 pt-10 w-svw gap-4 h-svh">
+      <Image
+        src={"/image/logo.svg"}
+        alt="Logo"
+        width={200}
+        height={200}
+      ></Image>
+      <form
+        onSubmit={handleSubmit}
+        method="post"
+        className="flex flex-col gap-4 w-1/2 lg:w-1/3 p-8 bg-slate-500 rounded-md shadow-2xl shadow-slate-800"
+      >
+        <InputLabel
+          id="username"
+          type="text"
+          label="Usuário"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <InputLabel
+          id="password"
+          type="password"
+          label="Senha"
+          value={formData.password}
+          onChange={handleChange}
+        />
 
+        {loading ? (
+          <span className="self-center">
+            <RiLoader2Fill size={30} />
+          </span>
+        ) : (
           <button
             type="submit"
-            className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full py-2 bg-pink-600 text-white rounded-md hover:bg-pink-800 focus:outline-none focus:ring-2 focus:ring-pink-600 shadow-lg shadow-slate-600"
           >
             Entrar
           </button>
-        </form>
-        <div className="mt-4 text-center">
-          <button
-            onClick={() => router.push("/users")}
-            className="text-blue-500 hover:underline"
-          >
-            Não tem uma conta? Cadastre-se
-          </button>
-        </div>
-      </div>
-    </div>
+        )}
+
+        {error && <span className="self-center">{error}</span>}
+      </form>
+    </main>
   );
 }
 
