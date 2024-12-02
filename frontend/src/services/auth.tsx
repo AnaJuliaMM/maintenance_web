5625371bf637e4cf72e9117611165c90ad98b2a5
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-12-02 07:54:22 -0300

    feat: create auth service

---
+++ frontend/src/services/auth.tsx
@@ -0,0 +1,48 @@
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from "axios";
+
+import { authType } from "@/types/authType";
+
+class AuthService {
+  private api: AxiosInstance;
+
+  constructor() {
+    this.api = axios.create({
+      baseURL: "http://localhost:3011/api/Auth",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  async login(endpoint: string, data: authType): Promise<AxiosResponse> {
+    try {
+      const response = await this.api.post(endpoint, data);
+      localStorage.setItem("token", response.data);
+      return response.data;
+    } catch (error: any) {
+      this.handleError(error);
+      throw error;
+    }
+  }
+
+  static async logout() {
+    localStorage.removeItem("token");
+  }
+
+  private handleError(error: AxiosError): void {
+    if (error.response) {
+      console.error(error.response);
+
+      console.error(
+        `Erro na resposta: ${error.response.status}`,
+        error.response.data
+      );
+    } else if (error.request) {
+      console.error("Erro na requisição:", error.request);
+    } else {
+      console.error("Erro ao configurar a requisição:", error.message);
+    }
+  }
+}
+
+export default new AuthService();
