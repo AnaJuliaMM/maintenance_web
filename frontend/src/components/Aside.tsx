d6df53b27183a068ca8fd1bd572f504520bfe4dc
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-12-10 20:54:26 -0300

    feat(aside): add logout button

--- frontend/src/components/Aside.tsx
+++ frontend/src/components/Aside.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 
 import { LuWarehouse } from "react-icons/lu";
 import { PiWashingMachine } from "react-icons/pi";
-import { FaUsers } from "react-icons/fa";
 import { IoIosListBox } from "react-icons/io";
 
 import { NavIconItem } from "./NavIconItem";
+
+import AuthService from "@/services/auth";
 
 export const Aside = () => {
   const navItems = [
@@ -28,21 +29,20 @@ export const Aside = () => {
     {
       value: "Usuários",
       href: "/users",
-      icon: <FaUsers />,
+      icon: <IoIosListBox />,
     },
   ];
 
   return (
-    <aside className="w-1/5 p-6 bg-gray-900/35 fixed h-svh">
-      <Image
-        src={"/image/logo.svg"}
-        alt="Logo"
-        width={200}
-        height={200}
-        className="w-full"
-        priority
-      ></Image>
+    <aside className="flex flex-col justify-between w-1/5 p-6 bg-gray-900/35 fixed h-svh">
       <nav className="flex flex-col space-y-4 mt-8">
+        <Image
+          src={"/image/logo.svg"}
+          alt="Logo"
+          width={200}
+          height={200}
+          className="w-full"
+        ></Image>
         {navItems.map((item, index) => (
           <NavIconItem
             href={item.href}
@@ -52,6 +52,16 @@ export const Aside = () => {
           />
         ))}
       </nav>
+
+      <button
+        onClick={() => {
+          AuthService.logout();
+          window.location.href = "/";
+        }}
+        className="flex justify-center gap-2 self-end  bg-blue-500 rounded-md font-semibold text-sm w-full py-2"
+      >
+        Sair
+      </button>
     </aside>
   );
 };
