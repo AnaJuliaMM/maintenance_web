50d41a205743e29910085ddf1a80f09dfd63b57a
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-12-11 07:43:20 -0300

    feat(type): create userrole type

--- frontend/src/Types/userType.ts
+++ frontend/src/Types/userType.ts
@@ -4,4 +4,15 @@ export type userType = {
   email: string;
   username: string;
   password: string;
+  role?: {
+    id: number;
+    name: UserRole;
+  };
 };
+
+export type UserRole =
+  | "user:admin"
+  | "user:user"
+  | "user:viewer"
+  | "user:editor"
+  | null;
