025947140b760d5b79c249e5577070e549b03aa1
Ana Julia M Martins <ana.martins_2004@outlook.com>
2024-12-15 00:12:17 -0300

    feat: create jwt payload type

--- frontend/src/Types/authType.ts
+++ frontend/src/Types/authType.ts
@@ -1,5 +1,19 @@
 export type authType = {
-    username: string;
-    password: string;
-  };
-  
+  username: string;
+  password: string;
+};
+
+export type JwtPayloadType = {
+  username: string;
+  role: userRole;
+  exp?: number;
+  iss?: string;
+  aud?: string;
+};
+
+export type userRole =
+  | "user:admin"
+  | "user:user"
+  | "user:viewer"
+  | "user:editor"
+  | null;
